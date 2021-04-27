3292d7895dcb0fb8eac93559ee3208156853b360
ThierryDC <dthierry.talis.bs@gmail.com>
2021-04-27 15:54:28 +0200

    modif product.php

--- product.php
+++ product.php
@@ -1,5 +1,29 @@
 <?php include('inc/header.php'); ?>
 
-    <h1>Page d'un produit</h1>
+<div  id="product">
+        <div class="wrapper">
+            <div class="content">
+                <div id="nameproduct">
+                    <h1>Une villa à la campagne</h1>
+                </div>
+                <div id="photo">
+                    <img src="./assets/img/photo.jpg" alt="une guitare">
+                </div>
+                <div id="info">
+                    <div id="price">600€</div>
+                    <div id="description">
+                        <p>Description du bien</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam architecto, accusamus harum incidunt voluptates natus in aut nisi tenetur neque rerum officiis dolorem recusandae quos culpa qui facere eveniet soluta?</p>
+                    </div>
+                    <div>
+                        <div class="td_button">Réserver</div>
+                    </div>
+                </div>
+                
+                
+            </div>
+        </div>
+    </div>
+
 
 <?php include('inc/footer.php'); ?>
