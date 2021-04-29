582bbabc993337dd9c3df6d1e4e96bb558fc884c
ThierryDC <dthierry.talis.bs@gmail.com>
2021-04-29 16:22:07 +0200

    add addproduct.php

---
+++ addproducts.php
@@ -0,0 +1,30 @@
+<?php include('inc/header.php'); ?>
+
+<?php
+    /**
+     * ! Créer un nouvel article à partir du formulaire.
+     * 
+     * TODO : Vérification intro : si le bouton est cliqué et si le formulaire est rempli
+     * 
+     * TODO : Initialisation des variables & assainissement
+     * 
+     * TODO : Vérification du prix positif
+     * 
+     * TODO : Enregistrement des données
+     */
+
+?>
+
+<div id="addproducts">
+    <div class="container">
+        <div class="columns is-centered">
+            <div class="column is-4">
+                <div class="content">
+                    <h1 class="block">Ajout de produits</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('inc/footer.php'); ?>
