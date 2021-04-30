5e8759c5e923d10a701fcdced6b9d6ec91de9c73
ThierryDC <dthierry.talis.bs@gmail.com>
2021-04-30 08:39:13 +0200

    product.php add form

--- product.php
+++ product.php
@@ -1,29 +1,20 @@
 <?php include('inc/header.php'); ?>
 
-<div  id="product">
-        <div class="wrapper">
-            <div class="content">
-                <div>
-                    <div id="nameproduct">
-                        <h1>Une villa à la campagne</h1>
-                    </div>
-                    <div id="photo">
-                        <img src="./assets/img/photo.jpg" alt="une maison">
-                    </div>
-                </div>
-                
-                <div id="info">
-                    <div id="price">600€</div>
-                    <div id="description">
-                        <p>Description du bien</p>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam architecto, accusamus harum incidunt voluptates natus in aut nisi tenetur neque rerum officiis dolorem recusandae quos culpa qui facere eveniet soluta?</p>
-                    </div>
-                    <div>
-                        <div class="td_button">Réserver</div>
-                    </div>
+<div id="product">
+    <div class="container">
+        <div class="columns is-centered">
+            <div class="column is-4">
+                <div class="content">
+                    <h1 class="block"><?php echo $product['title']; ?></h1>
+                    <p>A propos : <?php echo $product['description']; ?></p>
+                    <p>Nombre de chambres : <?php echo $product['bedroom_number']; ?></p>
+                    <p>Surface en m2 : <?php echo $product['surface']; ?></p>
+                    <p>Prix de la location à la semaine : <?php echo $product['price']; ?></p>
+                    <p>Catégorie du bien : <?php echo $product['category']; ?></p>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 <?php include('inc/footer.php'); ?>
--- products.php
+++ products.php
@@ -1,12 +1,22 @@
 <?php include('inc/header.php'); ?>
 
-    <div  id="products">
-        <div class="wrapper">
-            <div>
-                <h1>Page des Produits</h1>
-                
+<div id="products">
+    <div class="container">
+        <div class="columns is-centered">
+            <div class="column is-4">
+                <div class="content">
+                    <div class="card">
+                        <h5>Appartement dans Bordeaux</h5>
+                        <p>Catégorie du bien : Appartement</p>
+                        <p>Nombre de chambres : 3</p>
+                        <p>Surface en m2 : 120m2</p>
+                        <p>Prix de la location à la semaine : 600€</p>
+                        
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+</div>
 
 <?php include('inc/footer.php'); ?>
