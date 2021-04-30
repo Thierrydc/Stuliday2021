3aaae1bba91240a829d657c79e868a87e1afb87f
ThierryDC <dthierry.talis.bs@gmail.com>
2021-04-30 09:21:11 +0200

    products.php (add) SELECT BDD

--- products.php
+++ products.php
@@ -1,18 +1,41 @@
 <?php include('inc/header.php'); ?>
 
+<?php
+//! Affichage de tous les produits. Il faudra une requête SQL qui récupère tous les produits, et qui les affiche dans des cartes séparées.
+
+//? Création de ma requête SQL. Vu que j'ai des colonne qui font référence à d'autres tables, je dois ajouter des jointures sur category et author.
+$sth = $connect->query(
+    "SELECT title, c.name as category, bedroom_number, surface, price
+    FROM biens as b 
+    LEFT JOIN categories as c ON b.category_id=c.id
+    LEFT JOIN users as u ON b.author_id = u.id
+    ");
+
+//? Le résultat de ma requête est affiché dans un tableau associatif à l'aide du chaînage de méthodes.
+$locations = $sth->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($locations);
+?>
+
+
 <div id="products">
     <div class="container">
         <div class="columns is-centered">
             <div class="column is-4">
                 <div class="content">
-                    <div class="card">
-                        <h5>Appartement dans Bordeaux</h5>
-                        <p>Catégorie du bien : Appartement</p>
-                        <p>Nombre de chambres : 3</p>
-                        <p>Surface en m2 : 120m2</p>
-                        <p>Prix de la location à la semaine : 600€</p>
-                        
-                    </div>
+                    <?php
+                        foreach ($locations as $location) {
+                    ?>
+                            <div class="card">
+                            <h5><?php echo $location['title'] ?></h5>
+                            <p>Catégorie du bien : <?php echo $location['category'] ?></p>
+                            <p>Nombre de chambres : <?php echo $location['bedroom_number'] ?></p>
+                            <p>Surface en m2 : <?php echo $location['surface'] ?>m2</p>
+                            <p>Prix de la location à la semaine : <?php echo $location['price'] ?>€</p>
+                            </div>
+                    <?php
+                        }
+                    ?>
+                    
                 </div>
             </div>
         </div>
