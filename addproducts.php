7f1daea3db78efabf36cb992350e1f7be66a1153
ThierryDC <dthierry.talis.bs@gmail.com>
2021-04-30 08:12:52 +0200

    addproducts.php modify for location

--- addproducts.php
+++ addproducts.php
@@ -16,13 +16,14 @@
     $categories = $connect->query('SELECT * FROM categories')->fetchAll();
 
     //Vérification intro : si le bouton est cliqué et si le formulaire est rempli
-    if(isset($_POST['submit']) && !empty($_POST['name']) && !empty($_POST['description']) && !empty($_POST['price']) && !empty($_POST['category'])){
+    if(isset($_POST['submit']) && !empty($_POST['title']) && !empty($_POST['description']) && !empty($_POST['price']) && !empty($_POST['category']) && !empty($_POST['bedroom_number'])){
         
         //Initialisation des variables & assainissement
-        $name = strip_tags($_POST['name']);
+        $title = strip_tags($_POST['title']);
         $description = strip_tags($_POST['description']);
         $price = intval(strip_tags($_POST['price']));
         $category = strip_tags($_POST['category']);
+        $bedroom_number = intval(strip_tags($_POST['bedroom_number']));
         $user_id = $_SESSION['id'];
 
         //Vérification du prix positif
@@ -31,18 +32,20 @@
             try {
                //? Préparation de la requête, je définis la requête à exécuter avec des valeurs génériques (des paramètres nommés).
                $sth = $connect->prepare(
-                   "INSERT INTO products (name, description, price, category_id, author_id) VALUES (:name, :description, :price, :category, :author)"
+                   "INSERT INTO biens (title, description, bedroom_number, surface, price, category_id, author_id) VALUES (:title, :description, :bedroom_number, :surface, :price, :category, :author)"
                 );
                //? J'affecte chacun des paramètres nommés à leur valeur via un bindValue. Cette opération me protège des injections SQL (en + de l'assainissement des variables)
-               $sth->bindValue(':name', $name);
+               $sth->bindValue(':title', $title);
                $sth->bindValue(':description', $description);
+               $sth->bindValue(':bedroom_number', $bedroom_number);
+               $sth->bindValue(':surface', $bedroom_number);
                $sth->bindValue(':price', $price);
                $sth->bindValue(':category', $category);
                $sth->bindValue(':author', $user_id);
 
                //? J'exécute ma requête SQL d'insertion avec execute()
                 $sth->execute();
-                echo "Votre article a bien été ajouté";
+                echo "Votre annonce a bien été ajoutée";
                 //? Je redirige vers la page des produits.
                 header('Location: products.php');
             } catch (PDOException $error) {
@@ -63,22 +66,30 @@
         <div class="columns is-centered">
             <div class="column is-12">
                 <div class="content">
-                    <h1 class="block">Ajout de produits</h1>
+                    <h1 class="block">Ajout d'une location</h1>
                     <form action="#" method="post">
                         <div class="field">
-                            <label for="inputName" required>Nom de l'article</label>
-                            <input class="input" type="text" name="name" id="inputName" required>
+                            <label for="inputTitle" required>Titre de votre annonce</label>
+                            <input class="input" type="text" name="title" id="inputTitle" required>
                         </div>
                         <div class="field">
-                            <label for="inputDescription">Description de l'article</label>
-                            <textarea class="input" name="description" id="inputDescription" required></textarea>
+                            <label for="inputDescription">Description du bien</label>
+                            <textarea class="input" type="text" name="description" id="inputDescription" required></textarea>
                         </div>
                         <div class="field">
-                            <label for="inputPrice">Prix de l'article</label>
+                            <label for="InputBedroomsNumber">Nombre de chambres</label>
+                            <input class="input" type="number" min="1" name="bedroom_number" id="InputBedroomsNumber" required></input>
+                        </div>
+                        <div class="field">
+                            <label for="InputSurface">Surface en m2</label>
+                            <input class="input" type="number" min="1" name="surface" id="InputSurface" required></input>
+                        </div>
+                        <div class="field">
+                            <label for="inputPrice">Prix de la location à la semaine</label>
                             <input class="input" type="number" min="1" max="999999" name="price" id="inputPrice" required>
                         </div>
                         <div class="field">
-                            <label for="inputCategory">Catégorie de l'article</label>
+                            <label for="inputCategory">Catégorie du bien</label>
                             <select class="input" name="category" id="inputCategory" required>
                             <?php
                                 foreach ($categories as $category) {
@@ -91,7 +102,7 @@
                         </div>
                         <hr>
                         <div class="field">
-                            <input class="button block" type="submit" name="submit" value="Ajouter l'article">
+                            <input class="button block" type="submit" name="submit" value="Ajouter l'annonce">
                         </div>
                     </form>
                 </div>
