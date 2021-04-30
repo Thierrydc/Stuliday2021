51e64441f61f83f25496568e2f39b4e624d00d94
ThierryDC <dthierry.talis.bs@gmail.com>
2021-04-30 10:38:20 +0200

    product.php TODO for SELECT BDD

--- product.php
+++ product.php
@@ -1,5 +1,14 @@
 <?php include('inc/header.php'); ?>
 
+<?php
+    //? J'insère la valeur de l'id de ma requête GET dans une variable qui va me servir à récupérer un produit depuis la BDD
+    //? Création de ma requête SQL. Vu que j'ai des colonnes qui font référence à d'autres tables, je dois ajouter des jointures sur category et author. Je rajoute aussi la condition WHERE products_id = {$id} afin de récupérer le produit souhaité
+    //? Le résultat de ma requête est affiché dans un tableau associatif à l'aide du chaînage de méthodes.
+
+
+?>
+
+<!-- //? Ici pas besoin de boucle, puisque je ne récupère qu'un seul produit. -->
 <div id="product">
     <div class="container">
         <div class="columns is-centered">
