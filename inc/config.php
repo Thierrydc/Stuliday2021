762ed3bca506afd7ef4465e77ea6bea5cfeb6c90
ThierryDC <dthierry.talis.bs@gmail.com>
2021-06-17 11:21:38 +0200

    modif conf.php

--- inc/config.php
+++ inc/config.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $connect = new PDO("mysql:host=localhost;dbname=stuliday", 'root','');
+    $connect = new PDO("mysql:host=localhost;dbname=stuliday2021", 'root','');
     // Définir le mode d'erreur de PDO sur Exception
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     session_start();
